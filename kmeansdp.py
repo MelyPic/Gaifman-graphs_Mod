30b6e9bae9fce2ce2d127178f386853f583866f5
MElyPiC <melypic@gmail.com>
2020-09-04 18:13:01 +0200

    fill B in Dim

--- kmeansdp.py
+++ kmeansdp.py
@@ -12,22 +12,18 @@ correspondiente media del cluster. =
 import numpy as np
 
 def Dim(i,m):
-	if 0 <= i < len(x) and 1 <= m < len(x): 
-		#j in (m, i) Dim(j-1,m-1) +dxjxi(j,i)
-		minm = Dim(m-1,m-1) + dxjxi(m,i)
-		for j in range(m+1,i):
-			aux = Dim(j-1,m-1) + dxjxi(j,i)
-			if aux < minm:
-				minm = aux
-		return minm
-	elif m == 0:
-		return 0
-	else:		
-		print('Sale de rango, i,m: ', i,m)
-		return -1	
+	#0 <= i < len(x) and 1 <= m < len(x):
+	minm = B[m-1][m-1] + dxjxi(m,i)
+	#minm = Dim(m-1,m-1,B) + dxjxi(m,i)
+	for j in range(m+1,i):
+		aux = B[j-1][m-1] + dxjxi(j,i)#Dim(j-1,m-1) + dxjxi(j,i)
+		if aux < minm:
+			minm = aux
+	B[i][m] = minm		
+	return minm
+	
 		
 def dx1xi(i):
-	#print('dx1xi: ',i)
 	if i ==0:
 		return 0
 	else:
@@ -50,58 +46,40 @@ def miu(i):
 	else:
 		return (x[i]+(i-1)*miu(i-1))/i
 	
-def Bim(i,m):
-	arrayD = []
-	for j in range(m,i):
-		arrayD.append(Dim(j-1,m-1)+ dxjxi(j,i))
-	print (arrayD)
-	if arrayD ==[]:
-		return 0
-	else:
-		return np.argmin(arrayD, axis=0)	
-	#argmin = Dim(m-1,m-1) + dxjxi(m-1,i)
-	#print ('argmin: ',argmin)
-	#for j in range(m+1,i):
-	#	aux = Dim(j-1,m-1) + dxjxi(j,i)
-	#	if aux < argmin:
-	#		argmin = aux
-	#return argmin
+#def Bim(i,m):
+	#arrayD = []
+	#for j in range(m,i):
+		#arrayD.append(Dim(j-1,m-1)+ dxjxi(j,i))
+	#if arrayD ==[]:
+		#return 0
+	#else:
+		#return np.argmin(arrayD, axis=0)	
+
 	
 
-x=[]	
+x=[]
+B=[]
+	
 def main():
 	global x
 	X = [25, 1 ,20, 1, 10, 10]
 	x = X
 	x.sort()
 	print ('x:',x)
+	
 	k = 2 #clusters
-
 	n = len(x)
-	#minaux= Dim[1,1]
-	#for i in (1,n):
-	#	for m in (1,k):
-	#		aux = Dim[i,m]
-	#		if aux < minaux:
-	#			minaux = aux
-				
-	#llena matrix B
-	B = []
-	D = []
-	for i in range(1,n):
-		B.append([])
-		D.append([])
+	
+	for r in range(len(x)):
+		B.append([0]*k) 
+	
+	
+	for i in range(n):
 		for m in range(1,k):
-			#B[i-1].append(m) 
-			#print('i,m: ',i,m)
-			D[i-1].append(Dim(i,m))
-			B[i-1].append(Bim(i,m))
-	print ('X: ',X)		
-	print ('D: ',D)
-	print ('B: ')
+			Dim(i,m)
+			
 	for i in B:
 		print(i)
-	#print(Dim(n,k))
 	
 main()	
 	
